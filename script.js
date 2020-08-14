87ed64f1d8d8e1e3111b9c93759c8b427b141a28
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-14 08:52:19 +0300

    styled the html a little bit and added collection of objects to script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+//Steps create a collections of objects.
+const books = [{
+            title: "Harry Potter and the Philosopher Stone",
+            author: "Jk Rolling",
+            genre: "fantasy",
+            pages: 323,
+            reade: false,
+            id: 1597384168578,
+        },
+        {
+            title: "pachicnko",
+            author: "Min Jin Lee",
+            genre: "fiction",
+            pages: 400,
+            reade: false,
+            id: 1597384192030,
+        },
+        {
+            title: "Educated",
+            author: "Tara Westover",
+            genre: "Memory",
+            pages: 423,
+            reade: false,
+            id: 597384226575,
+        },
+    ]
+    //Create a html based on the collections.
